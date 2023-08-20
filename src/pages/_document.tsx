38a0c378f90d583006244c7572ec0fb66b39d2c1
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-08-19 23:18:41 -0300

    estilos globais

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 // Componente para alterar o html global - seria o index.html que ficava na pasta public
 // a tag main seria onde as paginas seriam renderizadas - o div com id root
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
 
 export default function Document() {
   return (
@@ -15,6 +16,11 @@ export default function Document() {
         <link
           href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@400;500;600;700;800&family=Poppins:wght@100;200;300;400;500;600;700;900&family=Roboto+Mono:wght@700&family=Roboto:wght@400;700&display=swap"
           rel="stylesheet"
+        />
+
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
         />
       </Head>
       <body>
