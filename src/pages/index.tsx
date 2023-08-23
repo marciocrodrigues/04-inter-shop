042d30a52081a3c5a357a3b0acf6d6a525a30751
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-08-23 00:09:39 -0300

    criando rotas e chamando, criando tela de detalhes do produto, mais conceitos de SSG e conceito de getStaticPaths e fallback

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { GetStaticProps } from 'next'
+import Link from 'next/link'
 import Stripe from 'stripe'
 import { stripe } from '../lib/stripe'
 import { useKeenSlider } from 'keen-slider/react'
@@ -13,7 +14,7 @@ interface HomeProps {
     id: string
     name: string
     imageUrl: string
-    price: number
+    price: string
   }[]
 }
 
@@ -29,13 +30,15 @@ export default function Home({ products }: HomeProps) {
     <HomeContainer ref={sliderRef} className="keen-slider">
       {products.map((product) => {
         return (
-          <Product key={product.id} className="keen-slider__slide">
-            <Image src={product.imageUrl} width={520} height={480} alt="" />
-            <footer>
-              <strong>{product.name}</strong>
-              <span>{product.price}</span>
-            </footer>
-          </Product>
+          <Link href={`/product/${product.id}`} key={product.id}>
+            <Product className="keen-slider__slide">
+              <Image src={product.imageUrl} width={520} height={480} alt="" />
+              <footer>
+                <strong>{product.name}</strong>
+                <span>{product.price}</span>
+              </footer>
+            </Product>
+          </Link>
         )
       })}
     </HomeContainer>
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,88 @@
+import { GetStaticPaths, GetStaticPathsResult, GetStaticProps } from 'next'
+import Image from 'next/image'
+import { stripe } from '../../lib/stripe'
+import Stripe from 'stripe'
+import { ParsedUrlQuery } from 'querystring'
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from '../../styles/pages/product'
 import { useRouter } from 'next/router'
 
-export default function Product() {
-  const { query } = useRouter()
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+  }
+}
 
-  return <h1>Product: {query.id}</h1>
+export default function Product({ product }: ProductProps) {
+  const { isFallback } = useRouter()
+
+  // Valida se o está carregando os dados da pagina no getStaticProps
+  if (isFallback) {
+    return <p>Loading</p>
+  }
+
+  return (
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+
+        <p>{product.description}</p>
+
+        <button>Comprar Agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async (): Promise<
+  GetStaticPathsResult<ParsedUrlQuery>
+> => {
+  return {
+    paths: [{ params: { id: 'prod_OUj4mig5OSlVy2' } }],
+    fallback: true,
+    // false = só carrega pagina se o parametro bater com os configurados nos paths
+    // true = carrega a pagina primeiro e tenta carregar o metodo getStaticProps ao mesmo tempo
+    // blocking = Só mostra a tela depois de executado o getStaticProps
+    // obs: optar pelo true e valida o isFallback com o useRouter e criar um loading
+  }
+}
+
+export const getStaticProps: GetStaticProps<unknown, { id: string }> = async ({
+  params,
+}) => {
+  const productId = params.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price'],
+  })
+
+  const price = product.default_price as Stripe.Price
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }).format(price.unit_amount / 100),
+        description: product.description,
+      },
+    },
+    revalidate: 60 * 60 * 1, // 1 hora
+  }
 }
