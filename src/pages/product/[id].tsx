08b3cb6c1940c25f1b77ffe380dae3ebbed5b6b8
Marcio Costa Rodrigues <marciodeth@hotmail.com>
2023-08-24 00:31:11 -0300

    checkout , componente de sucesso e redirecionamento

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -9,6 +9,8 @@ import {
   ProductDetails,
 } from '../../styles/pages/product'
 import { useRouter } from 'next/router'
+import axios from 'axios'
+import { useState } from 'react'
 
 interface ProductProps {
   product: {
@@ -17,15 +19,36 @@ interface ProductProps {
     imageUrl: string
     price: string
     description: string
+    defaultPriceId: string
   }
 }
 
 export default function Product({ product }: ProductProps) {
   const { isFallback } = useRouter()
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] =
+    useState(false)
 
   // Valida se o está carregando os dados da pagina no getStaticProps
   if (isFallback) {
     return <p>Loading</p>
+  }
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+
+      // para rota externa
+      window.location.href = checkoutUrl
+    } catch (err) {
+      setIsCreatingCheckoutSession(false)
+      alert('Falha ao redirecionar ao checkout')
+    }
   }
 
   return (
@@ -40,7 +63,9 @@ export default function Product({ product }: ProductProps) {
 
         <p>{product.description}</p>
 
-        <button>Comprar Agora</button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
+          Comprar Agora
+        </button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -81,6 +106,7 @@ export const getStaticProps: GetStaticProps<unknown, { id: string }> = async ({
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hora
